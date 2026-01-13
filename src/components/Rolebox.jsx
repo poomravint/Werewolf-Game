46e42d0d5241dc3ddd647681027fe767302e42ba
Pennapa Butda <poomravintwongwaro@gmail.com>
2026-01-13 16:23:35 +0700

    Add Select box

--- src/components/Rolebox.jsx
+++ src/components/Rolebox.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
+import "./Rolebox.css"
 
 const Rolebox = ({ Grouprole, setGroupRole }) => {
-
-const [role, setRole] = useState("");
+  const [role, setRole] = useState("");
+  const roleOptions = ["Villager", "Werewolf", "Seer"];
 
   return (
     <>
       <div className="Box-container">
         <p>Role box</p>
-        <select value={role} onChange={(e) => setRole(e.target.value)}>
+        <select className = "Select-box" value={role} onChange={(e) => setRole(e.target.value)}>
           <option value="">-- Select Role --</option>
-          <option value="Villager">Villager</option>
-          <option value="Werewolf">Werewolf</option>
-          <option value="Seer">Seer</option>
+          {roleOptions.map((r, index) => (
+            <option key={index} value={r}>
+              {r}
+            </option>
+          ))}
         </select>
       </div>
     </>
